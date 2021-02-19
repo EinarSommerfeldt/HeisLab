4041aaff073925c1b9fc4bf49bbda43c78c8bb8a
Einar Sommerfeldt <einar.sommerfeldt@gmail.com>
2021-02-19 12:34:17 +0000

    Removed unnecessary functions

--- skeleton_project/source/queue.h
+++ skeleton_project/source/queue.h
@@ -27,7 +27,7 @@ struct Queue {
 /**
  * @brief Constructor for the queue object.
  *
- * @return Returnerer a pointer to the new queue object.
+ * @return Pointer to the new queue object.
  */
 struct Queue* queue_new();
 
@@ -38,55 +38,30 @@ struct Queue* queue_new();
  * @param queue[in,out] Queue the floor will be added to.
  * @param elev[in] Elevator that owns the queue.
  * @param floor[in] Floor to be added.
+ * @param direction[in] Which direction the order is.
  */
-void queue_addToQueue(struct Queue* queue, struct Elevator* elev, int floor);
+void queue_addFloor(struct Queue* queue, struct Elevator* elev, int floor, int direction);
 
 /**
  * @brief Adds a node in the queue in front of the input node.
  *
  * @param queue[in,out] Queue the floor will be added to.
  * @param node[in,out] Node to be inserted.
+ * @param value[in] Value for the new node.
+ * 
+ * @return pointer to the new node 
  */
-void queue_insertNode(struct Queue* queue, struct Node* node);
+struct Node* queue_insertNode(struct Queue* queue, struct Node* node, int value);
 
 /**
- * @brief Removes a node from the queue, @ref deleteNode(struct Node* node) 
- * takes care of the memory deallocation.
+ * @brief Removes a node from the queue and frees the node from memory.
  *
  * @param queue[in,out] Queue the node will be removed from.
  * @param node[in,out] Node to be removed.
  * 
- * @return returns the @p value of the removed node.
  */
-int queue_removeNode(struct Queue* queue, struct Node* node);
+void queue_removeNode(struct Queue* queue, struct Node* node);
 
-/**
- * @brief Checks if the queue is empty.
- *
- * @param queue[in] Queue to be checked.
- * 
- * @return 1 if the queue is empty.
- */
-int queue_isEmpty(struct Queue* queue);
-
-/**
- * @brief Returns a pointer to the head of the queue.
- *
- * @param queue[in] Queue struct.
- * 
- * @return A pointer to the first node in the queue.
- */
-struct Node* queue_getHead(struct Queue* queue);
-
-/**
- * @brief Returns a pointer to the tail of the queue.
- * @warning This node is a dummy and serves only to point to the other nodes. To get the last viable floor use node->next
- *
- * @param queue[in] Queue struct.
- * 
- * @return A pointer to the last node in the queue.
- */
-struct Node* queue_getTail(struct Queue* queue);
 
 /**
  * @brief Removes the first node in the queue and returns it's value
@@ -95,13 +70,10 @@ struct Node* queue_getTail(struct Queue* queue);
  * 
  * @return Value of the first node.
  */
-int queue_popQueue(struct Queue* queue); 
+int queue_pop(struct Queue* queue); 
 
 //Node funksjoner
-struct Node* getPrev(struct Node* node);
-struct Node* getNext(struct Node* node);
-int getValue(struct Node* node);
-void deleteNode(struct Node* node);
+struct Node* node_new(int value);
 
 
 
